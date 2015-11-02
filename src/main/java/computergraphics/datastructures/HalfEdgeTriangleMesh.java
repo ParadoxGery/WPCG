f10205ad6423339b2bc628d09d4c2bd2f59b9ddd
Gery <gerhard.wagner@haw-hamburg.de>
2015-11-02 15:11:21 +0100

    class added

---
+++ src/main/java/computergraphics/datastructures/HalfEdgeTriangleMesh.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package computergraphics.datastructures;
+
+/**
+ * @author Gery
+ *
+ */
+public class HalfEdgeTriangleMesh implements ITriangleMesh {
+	
+	@Override
+	public void addTriangle(int vertexIndex1, int vertexIndex2, int vertexIndex3) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public int addVertex(Vertex v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public int getNumberOfTriangles() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public int getNumberOfVertices() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Vertex getVertex(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TriangleFacet getFacet(int facetIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void computeTriangleNormals() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTextureFilename(String filename) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTextureFilename() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
